23d1757b3c57129523ebd126ff17124dfadc4841
Maxence Guilbault <215420158+xqsweet@users.noreply.github.com>
2026-07-31 15:11:34 +0200

    test 8

--- main.py
+++ main.py
@@ -670,7 +670,7 @@ def main():
     yesterday_str = (paris_time - datetime.timedelta(days=1)).strftime("%d/%m/%Y")
 
     # Détection du jour (6 = Dimanche)
-    is_sunday = (paris_time.weekday() == 6)
+    is_sunday = True
     weekly_top_data = None
     
     if is_sunday:
